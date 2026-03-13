0e6b1d6ad99f5ba8b1fd4d0538987f3adc455c53
Doga Ozgulbas <dozgulbas@anl.gov>
2026-03-13 13:36:02 -0700

    Opensv patches

--- src/lerobot/teleoperators/openarm_gamepad/__init__.py
+++ src/lerobot/teleoperators/openarm_gamepad/__init__.py
@@ -1,10 +1,12 @@
 from .openarm_teleop_gamepad import OpenArmGamepadJointsTeleop
 from .openarm_teleop_bi_gamepad import OpenArmBiGamepadJointsTeleop
 from .openarm_configuration_gamepad import OpenArmGamepadJointsTeleopConfig, OpenArmBiGamepadJointsTeleopConfig
+from .openarm_hybrid_cartesian_gamepad import OpenArmHybridCartesianTeleop
 
 __all__ = [
     "OpenArmGamepadJointsTeleop",
     "OpenArmBiGamepadJointsTeleop", 
     "OpenArmGamepadJointsTeleopConfig",
-    "OpenArmBiGamepadJointsTeleopConfig"
+    "OpenArmBiGamepadJointsTeleopConfig",
+    "OpenArmHybridCartesianTeleop",
 ]
